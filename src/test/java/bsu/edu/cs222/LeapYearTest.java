5b3e5c5119d1ca09603d74cd72a4141973fa475e
Jack <jackwagneruni@gmail.com>
2025-08-28 10:26:03 -0400

    Added more test cases

--- src/test/java/bsu/edu/cs222/LeapYearTest.java
+++ src/test/java/bsu/edu/cs222/LeapYearTest.java
@@ -2,20 +2,26 @@ package bsu.edu.cs222;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-//import java.util.Scanner;
-
 
 public class LeapYearTest {
 
     @Test
     public void test2024isLeapYear() {
-        /* int inputedYear = new int; */
-        //Scanner userYearInput = new Scanner(System.in);
-        //System.out.print("Please enter the year: ");
-        //int inputedYear = userYearInput.nextInt();
-
-        int testYear1 = 200;
+        int testYear1 = 2024;
+        boolean result1 = LeapYearCalculator.isLeapYear(testYear1);
+        Assertions.assertTrue(result1);
+    }
+    @Test
+    public void test400isLeapYear() {
+        int testYear1 = 400;
+        boolean result1 = LeapYearCalculator.isLeapYear(testYear1);
+        Assertions.assertTrue(result1);
+    }
+    @Test
+    public void test2025isLeapYear() {
+        int testYear1 = 2025;
         boolean result1 = LeapYearCalculator.isLeapYear(testYear1);
         Assertions.assertFalse(result1);
     }
+
 }
