8ae4741ec81f0a4d1e45ed92c41abd9eb2888fed
Jack <jackwagneruni@gmail.com>
2025-08-28 10:36:37 -0400

    Added test case for dev by 100 but not 400

--- src/test/java/bsu/edu/cs222/LeapYearTest.java
+++ src/test/java/bsu/edu/cs222/LeapYearTest.java
@@ -13,15 +13,21 @@ public class LeapYearTest {
     }
     @Test
     public void test400isLeapYear() {
-        int testYear1 = 400;
-        boolean result1 = LeapYearCalculator.isLeapYear(testYear1);
-        Assertions.assertTrue(result1);
+        int testYear2 = 400;
+        boolean result2 = LeapYearCalculator.isLeapYear(testYear2);
+        Assertions.assertTrue(result2);
     }
     @Test
     public void test2025isLeapYear() {
-        int testYear1 = 2025;
-        boolean result1 = LeapYearCalculator.isLeapYear(testYear1);
-        Assertions.assertFalse(result1);
+        int testYear3 = 2025;
+        boolean result3 = LeapYearCalculator.isLeapYear(testYear3);
+        Assertions.assertFalse(result3);
+    }
+    @Test
+    public void test1900isLeapYear() {
+        int testYear4 = 1900;
+        boolean result4 = LeapYearCalculator.isLeapYear(testYear4);
+        Assertions.assertFalse(result4);
     }
 
 }
